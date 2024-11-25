d6fec16c35c4951971b2973a52c4e31d8015c6d5
CrescentKaz <121899324+CrescentKaz@users.noreply.github.com>
2024-11-25 12:12:08 -0600

    comment

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
-let balloonLocation = [];
+let balloonLocation = [Math.floor(Math.random())*100, Math.floor(Math.random())*100];
+let x = balloonLocation[0];
+let y = balloonLocation[1];
 
+balloon1.addEventListener(onclick, top = x, left = y);
