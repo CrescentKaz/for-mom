34ca23610c825ab5af42c8331d666a4887b8be58
CrescentKaz <121899324+CrescentKaz@users.noreply.github.com>
2024-11-26 12:02:37 -0600

    comment

--- script.js
+++ script.js
@@ -2,4 +2,12 @@ let balloonLocation = [Math.floor(Math.random())*100, Math.floor(Math.random())*
 let x = balloonLocation[0];
 let y = balloonLocation[1];
 
+const balloon1 = document.querySelector("#balloon1");
+const balloon2 = document.querySelector("#balloon2");
+const balloon3 = document.querySelector("#balloon3");
+const balloon4 = document.querySelector("#balloon4");
+const balloon5 = document.querySelector("#balloon5");
+
+
+
 // balloon1.addEventListener(onclick, top = x, left = y);
