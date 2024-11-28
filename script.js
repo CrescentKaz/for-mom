b0349efa838968e0d520cf5bfbfcc1895c03c6dc
CrescentKaz <121899324+CrescentKaz@users.noreply.github.com>
2024-11-27 19:07:44 -0600

    comment

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 let balloonLocation = [Math.floor(Math.random())*100, Math.floor(Math.random())*100];
-let x = balloonLocation[0];
-let y = balloonLocation[1];
+let leftMove = balloonLocation[0];
+let topMove = balloonLocation[1];
 
 const balloon1 = document.querySelector("#balloon1");
 const balloon2 = document.querySelector("#balloon2");
 const balloon3 = document.querySelector("#balloon3");
 const balloon4 = document.querySelector("#balloon4");
 const balloon5 = document.querySelector("#balloon5");
+
+function balloonMove(whichBalloon) {
+    whichBalloon.style.left = "leftMove vw";
+    whichBalloon.style.top = "topMove vh";
+}
 
 
 
