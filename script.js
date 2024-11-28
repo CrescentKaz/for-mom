6814e17d673b3f75542bee924d9e550def9cf8c1
CrescentKaz <121899324+CrescentKaz@users.noreply.github.com>
2024-11-27 21:42:46 -0600

    comment

--- script.js
+++ script.js
@@ -7,8 +7,17 @@ const balloon3 = document.getElementById("#balloon3");
 const balloon4 = document.getElementById("#balloon4");
 const balloon5 = document.getElementById("#balloon5");
 
+function moveLocation(ball) {
+    ball.style.top = move + "vh"; 
+    ball.style.left = Math.floor(Math.random()*100) + "vw";
+}
+
+balloon1.addEventListener("click", function(balloon1){console.log(move)});
+
+/*
 balloon1.addEventListener("click", function() {balloon1.style.top = move+"vh"; balloon1.style.left = move+"vw"});
 balloon2.addEventListener("click", function() {balloon2.style.top = "move"; balloon2.style.left = "move"});
 balloon3.addEventListener("click", function() {balloon3.style.top = "move"; balloon3.style.left = "move"});
 balloon4.addEventListener("click", function() {balloon4.style.top = "move"; balloon4.style.left = "move"});
 balloon5.addEventListener("click", function() {balloon5.style.top = "move"; balloon5.style.left = "move"});
+*/
