0488874c66073dfdfc34c85ebf52536ecbe8ec82
CrescentKaz <121899324+CrescentKaz@users.noreply.github.com>
2024-11-27 21:15:37 -0600

    comment

--- script.js
+++ script.js
@@ -1,24 +1,14 @@
-let balloonLocation = [Math.floor(Math.random())*100, Math.floor(Math.random())*100];
-let leftMove = balloonLocation[0];
-let topMove = balloonLocation[1];
+let move = Math.floor(Math.random()*100);
+console.log(move);
 
 const balloon1 = document.getElementById("#balloon1");
-const balloon2 = document.querySelector("#balloon2");
-const balloon3 = document.querySelector("#balloon3");
-const balloon4 = document.querySelector("#balloon4");
-const balloon5 = document.querySelector("#balloon5");
+const balloon2 = document.getElementById("#balloon2");
+const balloon3 = document.getElementById("#balloon3");
+const balloon4 = document.getElementById("#balloon4");
+const balloon5 = document.getElementById("#balloon5");
 
-/*
-function balloonMove() {
-    style.left = "leftMove vw";
-    style.top = "topMove vh";
-}
-
- balloon1.addEventListener("click", balloonMove());
- balloon2.addEventListener("click", balloonMove());
- balloon3.addEventListener("click", balloonMove());
- balloon4.addEventListener("click", balloonMove());
- balloon5.addEventListener("click", balloonMove());
-*/
-
- balloon5.addEventListener("click", function() {balloon5.style.top = "topMove vh"; balloon5.style.left = "leftMove vw"});
+balloon1.addEventListener("click", function() {balloon1.style.top = "move"; balloon1.style.left = "move"});
+balloon2.addEventListener("click", function() {balloon2.style.top = "move"; balloon2.style.left = "move"});
+balloon3.addEventListener("click", function() {balloon3.style.top = "move"; balloon3.style.left = "move"});
+balloon4.addEventListener("click", function() {balloon4.style.top = "move"; balloon4.style.left = "move"});
+balloon5.addEventListener("click", function() {balloon5.style.top = "move"; balloon5.style.left = "move"});
